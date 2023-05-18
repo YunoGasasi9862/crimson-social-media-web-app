915c95abb165200fdb51e93ce4aef9eb6a26fcd6
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-05-18 13:53:55 +0300

    Setting up

---
+++ dbScripts/Authentication.sql
@@ -0,0 +1,17 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_turkish_ci NOT NULL,
+  `profile` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `users` (`email`, `password`, `name`, `profile`) VALUES
+('root@gmail.com', '$2y$10$dV7UJZd8g9IKvmdjCwIlJeMKrdx2.2tU37aUf63PBwSJFTQL4u2sy', 'root', NULL);
+COMMIT;
