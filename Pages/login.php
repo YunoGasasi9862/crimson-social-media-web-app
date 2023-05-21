4dc9afd82483e8911e309ae595877d29fcc9b487
busecogen <91478384+busecogen@users.noreply.github.com>
2023-05-21 17:48:40 +0300

    Fixed Login Page
    Fixed the anomalities in login page

--- Pages/login.php
+++ Pages/login.php
@@ -3,7 +3,7 @@ require "../scripts/db.php";
 
 session_start(); //starts session
 
-if(isset($_SESSION['User']))  //checks if the user is already authenticated
+if (isset($_SESSION['User']))  //checks if the user is already authenticated
 {
   header("Location: Profile.php");
   exit;
@@ -11,14 +11,12 @@ if(isset($_SESSION['User']))  //checks if the user is already authenticated
 
 extract($_POST);
 
-if(!empty($_POST))
-{
+if (!empty($_POST)) {
   filter_var($email, FILTER_SANITIZE_SPECIAL_CHARS);
   filter_var($password, FILTER_SANITIZE_SPECIAL_CHARS);
-  $userLoggingIn= AuthenticateUser($email, $password);
-  if($userLoggingIn)
-  {
-    $_SESSION['User']=GetUser($email);  //sets up a session for that particular user
+  $userLoggingIn = AuthenticateUser($email, $password);
+  if ($userLoggingIn) {
+    $_SESSION['User'] = GetUser($email);  //sets up a session for that particular user
     header("Location: Profile.php");
     exit;
   }
@@ -27,74 +25,57 @@ if(!empty($_POST))
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Bootstrap/css/mdb.min.css">
-    <link rel="stylesheet" href="../css/index.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../Bootstrap/css/mdb.min.css">
+  <link rel="stylesheet" href="../css/index.css">
 
-    <title>Login</title>
+  <title>Login</title>
 </head>
-<body>
-<section class="vh-100 bg-image bg">
-  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-          <div class="card" style="border-radius: 15px;">
-            <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Sign in</h2>
 
-    <section class="vh-100 bg-image bg">
-        <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-          <div class="container h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-              <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                <div class="card" style="border-radius: 15px;">
-                  <div class="card-body p-5">
-                    <h2 class="text-uppercase text-center mb-5">Sign in</h2>
-      
-                    <form action="" method="post">
-                    <div class="form-floating mb-4">
+<body>
+
+  <section class="vh-100 bg-image bg">
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+            <div class="card" style="border-radius: 15px;">
+              <div class="card-body p-5">
+                <h2 class="text-uppercase text-center mb-5">Sign in</h2>
+
+                <form action="" method="post">
+                  <div class="form-floating mb-4">
                     <input name="email" type="email" class="form-control border-1" id="floatingInput1" placeholder="name@example.com">
                     <label for="floatingInput1">Email address</label>
                   </div>
 
-                  
+
                   <div class="form-floating mb-4">
                     <input name="password" type="password" class="form-control border-1" id="floatingInput1" placeholder="name@example.com">
                     <label for="floatingInput1">Password</label>
                   </div>
-      
+
                   <div class="d-flex justify-content-center">
-                    <button type="submit"
-                      class="btn btn-danger btn-block btn-lg gradient-custom-4 text-body">Sign in</button>
+                    <button type="submit" class="btn btn-danger btn-block btn-lg gradient-custom-4 text-body">Sign in</button>
                   </div>
-                  </form>
-      
-                  </div>
-                </div>
-              </div>
+                </form>
 
-              <div class="form-floating mb-4">
-                <input name="password" type="password" class="form-control border-1" id="floatingInput1" placeholder="name@example.com">
-                <label for="floatingInput1">Password</label>
               </div>
-
-              <div class="d-flex justify-content-center">
-                <button type="submit"
-                  class="btn btn-danger btn-block btn-lg gradient-custom-4 text-body">Sign in</button>
-              </div>
-            </form>
-            
             </div>
           </div>
+
         </div>
       </div>
     </div>
-  </div>
-</section>
-          
+    </div>
+    </div>
+    </div>
+  </section>
+
 </body>
+
 </html>
