943467a84cedb8c9886e9e8f65c998520243367d
busecogen <91478384+busecogen@users.noreply.github.com>
2023-05-21 19:30:02 +0300

    Date Operation Added
    Fixed posts database so that it will show the current timestamp for the added posts, fixed frontend of the feed page, added a past date option to posts so that user can see how much time past since the post is shared

--- Auth/posts.sql
+++ Auth/posts.sql
@@ -1,6 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1:3306
+-- Üretim Zamanı: 21 May 2023, 15:36:23
+-- Sunucu sürümü: 8.0.31
+-- PHP Sürümü: 8.0.26
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+03:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `crimson`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `posts`
+--
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
@@ -11,17 +36,36 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `image` varchar(500) NOT NULL,
   `comments` int NOT NULL,
   `likes` int NOT NULL,
-  `date` timestamp NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `postid` (`postid`),
   KEY `likes` (`likes`),
   KEY `date` (`date`),
   KEY `comments` (`comments`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Tablo döküm verisi `posts`
+--
+
+INSERT INTO `posts` (`id`, `postid`, `username`, `post`, `image`, `comments`, `likes`) VALUES
+(1, 1234, 'busecgn', 'this is a description', '', 0, 0),
+(2, 2393, 'root2525', 'sdsfdsf', '', 0, 0),
+(4, 5724, 'root2525', 'hello', '', 0, 0),
+(5, 1138603552241559876, 'root2525', 'this is my cousin', '', 0, 0),
+(6, 1044, 'root2525', 'this is a desc', '', 0, 0),
+(7, 756013879280, 'root2525', 'I am buse', '', 0, 0);
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `posts`
+--
 ALTER TABLE `posts` ADD FULLTEXT KEY `post` (`post`);
-
-INSERT INTO `posts`(`postid`, `username`, `post`, `comments`, `likes`) 
-VALUES ('1234','busecgn','this is a description','0','0');
-
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
