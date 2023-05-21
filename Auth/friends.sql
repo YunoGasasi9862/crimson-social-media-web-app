c150af2c6d2d6c9b6ee25383e2fe4474e7ce31ee
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-05-22 00:55:28 +0300

    DB

--- Auth/friends.sql
+++ Auth/friends.sql
@@ -2,14 +2,13 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+03:00";
 
-DROP TABLE IF EXISTS `friendsposts`;
-CREATE TABLE IF NOT EXISTS `friendsposts` (
-  `id` bigint NOT NULL,
+DROP TABLE IF EXISTS `friends`;
+CREATE TABLE IF NOT EXISTS `friends` (
   `userEmail` varchar(500) NOT NULL,
   `FriendEmail` varchar(500) NOT NULL,
   PRIMARY KEY (`id`),
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `friendsposts` (`id`, `userEmail`, `FriendEmail`) VALUES
-(1,"", "")
+INSERT INTO `friendsposts` (`userEmail`, `FriendEmail`) VALUES
+("m.bilal9862@gmail.com", "iman@gmail.com")
 COMMIT;
