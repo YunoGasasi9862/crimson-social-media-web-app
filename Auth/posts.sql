405194ddfdda590d7ab167d3e1cc064d0de0db2d
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-05-27 17:10:14 +0300

    dbTypechanges

--- Auth/posts.sql
+++ Auth/posts.sql
@@ -5,12 +5,12 @@ SET time_zone = "+03:00";
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `postid` bigint NOT NULL,
+  `postid` varchar(100) NOT NULL,
   `username` varchar(500) NOT NULL,
   `post` text NOT NULL,
   `image` varchar(500) DEFAULT NULL,
-  `comments` int NOT NULL,
-  `likes` int NOT NULL,
+  `comments` varchar(500),
+  `likes` varchar(500),
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `postid` (`postid`),
