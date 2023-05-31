41b921217c1b0cc925d9b5dac259ee82a95daf8e
ImanZahid <iman.zahid@ug.bilkent.edu.tr>
2023-05-31 13:23:55 +0300

    Integrated the friend list with backend.

--- Classes/user.php
+++ Classes/user.php
@@ -1,19 +1,31 @@
 <?php
 require_once "../scripts/config.php";
-class User {
-    public static function get_user($username){
-        $db=new PDO(DSN, USER, PASS);
-        $query= "SELECT * FROM users where username=?"; 
-        $record= $db->prepare($query);
-        $record->execute([$username]);  
+class User
+{
+    public static function get_user($username)
+    {
+        $db = new PDO(DSN, USER, PASS);
+        $query = "SELECT * FROM users where username=?";
+        $record = $db->prepare($query);
+        $record->execute([$username]);
         return $record->fetch(PDO::FETCH_ASSOC);
     }
 
-    public static function get_user_like($input){
-        $db=new PDO(DSN, USER, PASS);
+    public static function get_users_by_username($usernames)
+    {
+        $db = new PDO(DSN, USER, PASS);
+        $query = "SELECT * FROM users WHERE username IN ($usernames)";
+        $record = $db->prepare($query);
+        $record->execute();
+        return $record->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function get_user_like($input)
+    {
+        $db = new PDO(DSN, USER, PASS);
         $query = "SELECT * FROM users WHERE username LIKE ? OR name LIKE ? OR surname LIKE ?";
         $record = $db->prepare($query);
-        $record->execute([$input . '%',$input . '%',$input . '%']);
+        $record->execute([$input . '%', $input . '%', $input . '%']);
         return $record->fetchAll(PDO::FETCH_ASSOC);
     }
 }
