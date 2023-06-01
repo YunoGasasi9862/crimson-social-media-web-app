fb089ee1da1159666a2a2709cc50aa9ea68ce936
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-06-01 09:33:21 +0300

    Revert "Merge pull request #19 from YunoGasasi9862/iman"
    This reverts commit f30be07dcf4acd28c49fbf8c85c199acfdcfe285, reversing
    changes made to 1fd9ecf111a62bc19fa2b32cc50cc496f04f8d76.

--- Pages/Feed.php
+++ Pages/Feed.php
@@ -11,7 +11,7 @@ include "../Classes/friends.php";
 // 
 $friends = Friends::getFriends($email); //returns all the friends
 $usernames = Friends::fetchUsernames($friends); //returns all the emails of the friends
-$friendsUsers = User::get_users_by_username($usernames);
+
 //now we need their usernames
 
 ?>
@@ -24,48 +24,41 @@ $friendsUsers = User::get_users_by_username($usernames);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
   </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-    integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <script>
-  function navigateToNextPage() {
-    const currentURL = window.location.href;
-    const url = new URL(currentURL);
-    let currentPage = parseInt(url.searchParams.get("page")) || 1;
-    const nextPage = currentPage + 1;
-    url.searchParams.set("page", nextPage);
-    window.location.href = url.toString();
-  }
+    function navigateToNextPage() {
+      const currentURL = window.location.href;
+      const url = new URL(currentURL);
+      let currentPage = parseInt(url.searchParams.get("page")) || 1;
+      const nextPage = currentPage + 1;
+      url.searchParams.set("page", nextPage);
+      window.location.href = url.toString();
+    }
   </script>
   <script src="../JS/post.js"></script>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="../css/mainpage.css">
   <link rel="stylesheet" href="../css/extracss.css">
 
-  <script>
-  localStorage.setItem("username",
-  '<?php echo $_SESSION['User']['username'] ?>'); //setting the username into localstorage
+<script>
+  localStorage.setItem("username", '<?php echo $_SESSION['User']['username'] ?>'); //setting the username into localstorage
 
   function toggleNotftable() {
     var notftable = document.querySelector(".notftable");
     notftable.classList.toggle("show");
   }
 
-
+  
   function togglefriendstable() {
     var friendstable = document.querySelector(".friendstable");
     friendstable.classList.toggle("show");
   }
-  </script>
+</script>
 
 </head>
 
@@ -81,68 +74,44 @@ $friendsUsers = User::get_users_by_username($usernames);
 
         </button>
 
-        <div class="notftable">
+              <div class="notftable">
+              
+                <div class="circle">3</div>
+        <button class="button" onclick="toggleNotftable()">
 
-          <div class="circle">3</div>
-          <button class="button" onclick="toggleNotftable()">
-
-            <img src="../img/notification.png" id="notf" alt="">
-          </button>
-
-          <div class="submain" id="submain">
-            <ul>
-              <br>
-              <h4>Notifications</h4>
-              <li><img src="../img/avatar-1.webp" id="pic1" alt="">
-                <div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd
-                    dddddddddddmenu wtrtÖğesi
-                    iiii </a></div>
-                <p></p>
-                <h1></h1>
-              </li>
-              <li><img src="../img/avatar-1.webp" id="pic1" alt="">
-                <div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd
-                    dddddddddddmenu wtrtÖğesi
-                    iiii </a></div>
-                <h1></h1>
-              </li>
-              <li><img src="../img/avatar-1.webp" id="pic1" alt="">
-                <div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd
-                    dddddddddddmenu wtrtÖğesi
-                    iiii </a></div>
-              </li>
-            </ul>
-
-          </div>
-
+          <img src="../img/notification.png" id="notf" alt="">
+        </button>
+      
+        <div class="submain" id="submain">
+          <ul>
+            <br><h4>Notifications</h4>
+            <li><img src="../img/avatar-1.webp" id="pic1" alt=""><div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi iiii </a></div><p></p> <h1></h1></li>
+            <li><img src="../img/avatar-1.webp" id="pic1"alt=""><div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi iiii </a></div> <h1></h1></li>
+            <li><img src="../img/avatar-1.webp" id="pic1"alt=""><div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi iiii </a></div></li>
+          </ul>
+          
         </div>
-        <div class="friendstable">
+      
+      </div>
+      <div class="friendstable">
+        
+        
+      <button class="frbutton" onclick="togglefriendstable()" style="border:none">
+        <img src="../img/friend.png" id="friend" alt="">
+      </button>
+      <div class="frmain" id="frmain">
+        <ul>
+          <br><h4>Friends</h4>
+          <li><img src="../img/avatar-1.webp" id="frpic1" alt=""><div class="frsquare"><a href="#">DEfne ersungur bilmemne </a><button class="remove-button">Remove</button></div> <h1></h1></li>
+          <li><img src="../img/avatar-1.webp" id="frpic1"alt=""><div class="frsquare"><a href="#">Subsdd iiii </a><button class="remove-button">Remove</button></div> <h1></h1></li>
+          <li><img src="../img/avatar-1.webp" id="frpic1"alt=""><div class="frsquare"><a href="#">Subsdddd iiii </a><button class="remove-button">Remove</button></div></li>
+        </ul>
+        
+      </div>
+  </div>
 
 
-          <button class="frbutton" onclick="togglefriendstable()" style="border:none">
-            <img src="../img/friend.png" id="friend" alt="">
-          </button>
-          <div class="frmain" id="frmain">
-            <ul>
-              <br />
-              <h4>Friends</h4>
-              <?php
-                            foreach ($friends as $friend) {
-                                echo '
-                                    <li>
-                                        <img src="../img/avatar-1.webp" id="frpic1" alt="" />
-                                        <div class="frsquare">
-                                            <a href="#">' . $friend['username'] . '</a>
-                                            <button class="remove-button">Remove</button>
-                                        </div>
-                                        <h1></h1>
-                                    </li>
-                                ';
-                            }
-                        ?>
-            </ul>
-          </div>
-        </div>
+
         <div class="nav-item">
           <a href="./SearchBar.php"><img src="../img/search.png" alt="" id="search"></a>
         </div>
@@ -152,31 +121,32 @@ $friendsUsers = User::get_users_by_username($usernames);
 
 
       <?php
-				const PAGE_SIZE = 10;
-				$countTest = 0;
-				$page = isset($_GET["page"]) ? $_GET["page"] : 1;
-				if ($usernames) {
-					$posts = array();
-					foreach ($usernames as $username) {
-						array_push($posts, ...Post::get_posts($username));
-					}
-					$numOfPosts = count($posts);
-					$totalPages = ceil($numOfPosts / PAGE_SIZE);
-					$requestedPage = $page > $totalPages || $page < 1 ? 1 : $page;
-					// $start = ($page - 1) * PAGE_SIZE;
-					// $end = min($start + PAGE_SIZE, $count);
-					$paginatedPosts = array_slice($posts, ($requestedPage - 1) * PAGE_SIZE, PAGE_SIZE);
-					foreach ($paginatedPosts as $row) {
-						$row_user = User::get_user($row["username"]);
-						$countTest++;
-						include "../HTML/post.php";
-					}
-				}
-				if ($countTest != 0) //checks if there are any posts to show: If so, then shows the next button
-				{
-					echo "<button class='neon-button' onclick='navigateToNextPage()'>Next</button>";
-				}
-			?>
+      const PAGE_SIZE = 10;
+      $countTest=0;
+      $page = isset($_GET["page"]) ? $_GET["page"] : 1;
+      if ($usernames) {
+        $posts = array();
+        foreach ($usernames as $username) {
+          array_push($posts, ...Post::get_posts($username));
+        }
+        $numOfPosts = count($posts);
+        $totalPages = ceil($numOfPosts / PAGE_SIZE);
+        $requestedPage = $page > $totalPages || $page < 1 ? 1 : $page;
+        // $start = ($page - 1) * PAGE_SIZE;
+        // $end = min($start + PAGE_SIZE, $count);
+        $paginatedPosts = array_slice($posts, ($requestedPage - 1) * PAGE_SIZE, PAGE_SIZE);
+        foreach ($paginatedPosts as $row) {
+          $row_user = User::get_user($row["username"]);
+          $countTest++;
+          include "../HTML/post.php";
+        }
+      }
+         if($countTest!=0) //checks if there are any posts to show: If so, then shows the next button
+         {
+          echo "<button class='neon-button' onclick='navigateToNextPage()'>Next</button>";
+         }
+      ?>
+      
     </div>
   </div>
 
