b0d8b3c57b5ed745c7958ac5154d8aea007f2454
ImanZahid <iman.zahid@ug.bilkent.edu.tr>
2023-06-01 13:59:53 +0300

    updated

--- Pages/Feed.php
+++ Pages/Feed.php
@@ -1,5 +1,7 @@
 <?php
-if(session_id() == ''){ session_start();}
+if (session_id() == '') {
+  session_start();
+}
 
 $user = $_SESSION['User']['username'];
 $email = $_SESSION['User']['email'];
@@ -7,12 +9,28 @@ include "../Classes/post.php";
 include "../Classes/user.php";
 include "../Classes/friends.php";
 
-//collect posts
-// 
+
+function filterCurrent($username)
+{
+  global $user;
+  return $username !== $user;
+}
+
 $friends = Friends::getFriends($email); //returns all the friends
 $usernames = Friends::fetchUsernames($friends); //returns all the emails of the friends
+$usernames = array_filter($usernames, "filterCurrent");
+$friendsUsers = array();
+foreach ($usernames as $username) {
+  array_push($friendsUsers, User::get_user($username));
+}
 
-//now we need their usernames
+$usersToShow = array();
+
+foreach ($friendsUsers as $friend) {
+  if ($friend) {
+    $usersToShow[] = $friend;
+  }
+}
 
 ?>
 
@@ -24,10 +42,14 @@ $usernames = Friends::fetchUsernames($friends); //returns all the emails of the 
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
-  </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+    integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <script>
     function navigateToNextPage() {
@@ -41,24 +63,27 @@ $usernames = Friends::fetchUsernames($friends); //returns all the emails of the 
   </script>
   <script src="../JS/post.js"></script>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="../css/mainpage.css">
   <link rel="stylesheet" href="../css/extracss.css">
 
-<script>
-  localStorage.setItem("username", '<?php echo $_SESSION['User']['username'] ?>'); //setting the username into localstorage
+  <script>
+    localStorage.setItem("username",
+      '<?php echo $_SESSION['User']['username'] ?>'); //setting the username into localstorage
 
-  function toggleNotftable() {
-    var notftable = document.querySelector(".notftable");
-    notftable.classList.toggle("show");
-  }
+    function toggleNotftable() {
+      var notftable = document.querySelector(".notftable");
+      notftable.classList.toggle("show");
+    }
 
-  
-  function togglefriendstable() {
-    var friendstable = document.querySelector(".friendstable");
-    friendstable.classList.toggle("show");
-  }
-</script>
+
+    function togglefriendstable() {
+      var friendstable = document.querySelector(".friendstable");
+      friendstable.classList.toggle("show");
+    }
+  </script>
 
 </head>
 
@@ -74,41 +99,72 @@ $usernames = Friends::fetchUsernames($friends); //returns all the emails of the 
 
         </button>
 
-              <div class="notftable">
-              
-                <div class="circle">3</div>
-        <button class="button" onclick="toggleNotftable()">
+        <div class="notftable">
 
-          <img src="../img/notification.png" id="notf" alt="">
-        </button>
-      
-        <div class="submain" id="submain">
-          <ul>
-            <br><h4>Notifications</h4>
-            <li><img src="../img/avatar-1.webp" id="pic1" alt=""><div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi iiii </a></div><p></p> <h1></h1></li>
-            <li><img src="../img/avatar-1.webp" id="pic1"alt=""><div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi iiii </a></div> <h1></h1></li>
-            <li><img src="../img/avatar-1.webp" id="pic1"alt=""><div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi iiii </a></div></li>
-          </ul>
-          
+          <div class="circle">3</div>
+          <button class="button" onclick="toggleNotftable()">
+
+            <img src="../img/notification.png" id="notf" alt="">
+          </button>
+
+          <div class="submain" id="submain">
+            <ul>
+              <br>
+              <h4>Notifications</h4>
+              <li><img src="../img/avatar-1.webp" id="pic1" alt="">
+                <div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi
+                    iiii </a></div>
+                <p></p>
+                <h1></h1>
+              </li>
+              <li><img src="../img/avatar-1.webp" id="pic1" alt="">
+                <div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi
+                    iiii </a></div>
+                <h1></h1>
+              </li>
+              <li><img src="../img/avatar-1.webp" id="pic1" alt="">
+                <div class="square"><a href="#">Subsddddddddddddddddddddddddddd dddddddddd dddddddddddmenu wtrtÖğesi
+                    iiii </a></div>
+              </li>
+            </ul>
+
+          </div>
+
         </div>
-      
-      </div>
-      <div class="friendstable">
-        
-        
-      <button class="frbutton" onclick="togglefriendstable()" style="border:none">
-        <img src="../img/friend.png" id="friend" alt="">
-      </button>
-      <div class="frmain" id="frmain">
-        <ul>
-          <br><h4>Friends</h4>
-          <li><img src="../img/avatar-1.webp" id="frpic1" alt=""><div class="frsquare"><a href="#">DEfne ersungur bilmemne </a><button class="remove-button">Remove</button></div> <h1></h1></li>
-          <li><img src="../img/avatar-1.webp" id="frpic1"alt=""><div class="frsquare"><a href="#">Subsdd iiii </a><button class="remove-button">Remove</button></div> <h1></h1></li>
-          <li><img src="../img/avatar-1.webp" id="frpic1"alt=""><div class="frsquare"><a href="#">Subsdddd iiii </a><button class="remove-button">Remove</button></div></li>
-        </ul>
-        
-      </div>
-  </div>
+        <div class="friendstable">
+
+
+          <button class="frbutton" onclick="togglefriendstable()" style="border:none">
+            <img src="../img/friend.png" id="friend" alt="">
+          </button>
+          <div class="frmain" id="frmain">
+            <ul>
+              <br>
+              <h4>Friends</h4>
+              <div style="overflow-y:scroll;max-height:300px;">
+                <?php
+                foreach ($usersToShow as $userToShow) {
+                  $username = $userToShow['username'];
+                  $name = $userToShow['name'];
+                  $surname = $userToShow['surname'];
+                  $picture = $userToShow["profile"] ? $userToShow["profile"] : "../img/avatar-1.webp";
+                  echo "
+                <li>
+                  <img src=\"$picture\" id=\"frpic1\" alt=\"\">
+                    <div class=\"frsquare\">
+                    <a href=\"#\">$name $surname</a>
+                    <button class=\"remove-button\">Remove</button>
+                    </div>
+                  <h1></h1>
+                </li>
+              ";
+                }
+                ?>
+              </div>
+            </ul>
+
+          </div>
+        </div>
 
 
 
@@ -122,7 +178,7 @@ $usernames = Friends::fetchUsernames($friends); //returns all the emails of the 
 
       <?php
       const PAGE_SIZE = 10;
-      $countTest=0;
+      $countTest = 0;
       $page = isset($_GET["page"]) ? $_GET["page"] : 1;
       if ($usernames) {
         $posts = array();
@@ -141,12 +197,12 @@ $usernames = Friends::fetchUsernames($friends); //returns all the emails of the 
           include "../HTML/post.php";
         }
       }
-         if($countTest!=0) //checks if there are any posts to show: If so, then shows the next button
-         {
-          echo "<button class='neon-button' onclick='navigateToNextPage()'>Next</button>";
-         }
+      if ($countTest != 0) //checks if there are any posts to show: If so, then shows the next button
+      {
+        echo "<button class='neon-button' onclick='navigateToNextPage()'>Next</button>";
+      }
       ?>
-      
+
     </div>
   </div>
 
